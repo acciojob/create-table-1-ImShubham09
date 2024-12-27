ca684580ca8529535ae994191bba3f711125627f
ImShubham09 <singhashubham612@gmail.com>
2024-12-27 20:23:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const newRow = table.insertRow(0);
 const cell1 = newRow.insertCell(0);
 const cell2 = newRow.insertCell(1);
 
-cell1.textContent = "New cell1";
-cell2.textContent = "New cell2";
+cell1.textContent = "New Cell1";
+cell2.textContent = "New Cell2";
 
 
   
